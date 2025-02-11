0710ef14dc33049c709a1f44a691107e00353e9e
Alfie Elliott <116003344+alfie-3@users.noreply.github.com>
2025-02-11 18:11:29 +0000

    Added some stuff for stocking

--- Assets/Scripts/Item/furniture_Item.cs
+++ Assets/Scripts/Item/furniture_Item.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Furniture", menuName = "Items/Placable/Funiture")]
 public class furniture : Placable_Item
 {
-    
+    private GameObject _PlaceablePrefab;
+
 }
---
+++ Assets/Scripts/Stocking/FurnitureBoxController.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class FurnitureBoxController : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+}
