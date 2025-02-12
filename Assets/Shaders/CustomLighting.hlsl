f78e7948cc7c2a3b60010affb63421641d7606bb
Greg <116003047+CalypsoGreevin@users.noreply.github.com>
2025-02-12 14:53:25 +0000

    Shader Updates

--- Assets/Shaders/CustomLighting.hlsl
+++ Assets/Shaders/CustomLighting.hlsl
@@ -28,5 +28,31 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
                 float shadowStrength = GetMainLightShadowStrength();
                 ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false);
             #endif
+    
+            
+    
         #endif
         }
+
+void AdditionalLights_float(float3 SpecColor, float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse, out float3 Specular)
+{
+    float3 diffuseColor = 0;
+    float3 specularColor = 0;
+
+#ifndef SHADERGRAPH_PREVIEW
+    Smoothness = exp2(10 * Smoothness + 1);
+    WorldNormal = normalize(WorldNormal);
+    WorldView = SafeNormalize(WorldView);
+    int pixelLightCount = GetAdditionalLightsCount();
+    for (int i = 0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+        specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+    }
+#endif
+
+    Diffuse = diffuseColor;
+    Specular = specularColor;
+}
