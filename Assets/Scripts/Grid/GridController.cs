87c188242191d22c6b9930574be1a57647b9be6b
MAXTHEMAD <91728226+MAXTHEMAD@users.noreply.github.com>
2025-02-27 13:19:09 +0000

    Grid Placement
    possible bug, must test further

---
+++ Assets/Scripts/Grid/GridController.cs
@@ -0,0 +1,86 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    Grid grid;
+    public static GridController Instance { get; private set; }
+    [SerializeField] bool objectInMiddleOfCell;
+    [SerializeField] Vector3 gridOffset;
+    
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        grid = GetComponent<Grid>(); 
+    }
+    [SerializeField] private Vector2 GizmoVec;
+
+    public Vector3 HitToGrid(Vector3 hitPosition)
+    {
+        Vector3 position = hitPosition + gridOffset;// + (grid.cellSize * 0.5f);
+        Debug.DrawLine(position,position+Vector3.up);
+        Vector3 gridPos = grid.WorldToCell(position);
+        position = Vector3.Scale(gridPos, grid.cellSize) + (objectInMiddleOfCell ? (grid.cellSize * 0.5f) : Vector3.zero);
+        position = new Vector3(position.x, hitPosition.y, position.z);
+        return position - gridOffset;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+#if UNITY_EDITOR
+        Gizmos.color = Color.blue;
+
+        //Draw the suspension
+        Gizmos.DrawLine(
+            transform.position,
+            Vector3.up
+        );
+        Vector3 pos = Vector3.zero;
+        for (pos.x = 0; pos.x < GizmoVec.x; pos.x++)
+        {
+            for (pos.z = 0; pos.z < GizmoVec.y; pos.z++)
+            {
+                Gizmos.DrawWireCube(Vector3.Scale(pos , GetComponent<Grid>().cellSize) + Vector3.Scale(pos , GetComponent<Grid>().cellGap) + transform.position + (GetComponent<Grid>().cellSize*0.5f) - (gridOffset), 
+                    GetComponent<Grid>().cellSize);
+            }
+        }
+        Gizmos.color = Color.green;
+        //Gizmos.DrawWireCube(Vector3.Scale(objectBounds, positionTest) + transform.position + gizmoOffset, objectBounds);
+
+        //draw force application point
+        Gizmos.DrawWireSphere(Vector3.zero, 0.05f);
+        Gizmos.color = Color.white;
+#endif
+    }
+}
+
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(GridController))]
+class CustomGridyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+
+        DrawDefaultInspector();
+
+        GridController gridy = (GridController)target;
+
+        if (GUILayout.Button("setGrid"))
+        {
+        }
+    }
+}
+#endif
--- Assets/Scripts/Stocking/FurnitureBoxController.cs
+++ Assets/Scripts/Stocking/FurnitureBoxController.cs
@@ -13,11 +13,18 @@ public class FurnitureBoxController : NetworkBehaviour, IUsePrimary, IUpdate
 
     public const float PLACABLE_DISTANCE = 5;
 
+    GridController grid;
     private void Awake() {
         renderParams = new RenderParams(HologramMat);
         
         if (furnitureItem != null)
             PopulateItem(furnitureItem);
+        grid = GridController.Instance;
+    }
+
+    public void Init(Grid grid)
+    {
+         
     }
 
     [Rpc(SendTo.Everyone)]
@@ -43,9 +50,12 @@ public class FurnitureBoxController : NetworkBehaviour, IUsePrimary, IUpdate
 
         Ray ray = new(holdingManager.CameraManager.CamTransform.position, holdingManager.CameraManager.CamTransform.forward);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, PLACABLE_DISTANCE, LayerMask.GetMask("Placeable")) && Physics.OverlapBox(hit.point + holoMesh.bounds.center, holoMesh.bounds.size * 0.48f).Length == 0)
+
+        if (Physics.Raycast(ray, out RaycastHit hit, PLACABLE_DISTANCE, LayerMask.GetMask("Placeable")))
         {
-            holdingManager.PlaceItem_Rpc(NetworkObject, furnitureItem.ItemID, hit.point, Quaternion.Euler(0, holdingManager.Rotation, 0));
+            Vector3 position = grid.HitToGrid(hit.point);
+            if (Physics.OverlapBox(position + holoMesh.bounds.center, holoMesh.bounds.size * 0.48f, Quaternion.Euler(0, holdingManager.Rotation, 0)).Length == 0)
+                holdingManager.PlaceItem_Rpc(NetworkObject, furnitureItem.ItemID, position, Quaternion.Euler(0, holdingManager.Rotation, 0));
         }
     }
 
@@ -57,14 +67,23 @@ public class FurnitureBoxController : NetworkBehaviour, IUsePrimary, IUpdate
         Ray ray = new(holdingManager.CameraManager.CamTransform.position, holdingManager.CameraManager.CamTransform.forward);
         if (Physics.Raycast(ray, out RaycastHit hit, PLACABLE_DISTANCE, LayerMask.GetMask("Placeable")))
         {
-            HologramMat.SetFloat("_OverlappingColliders", Physics.OverlapBox(hit.point + holoMesh.bounds.center, holoMesh.bounds.size * 0.48f).Length);
-            Graphics.RenderMesh(renderParams, holoMesh, 0, Matrix4x4.TRS((hit.point + furnitureItem.FurniturePrefab.transform.position), Quaternion.Euler(0, holdingManager.Rotation, 0), Vector3.one));
-            if (furnitureItem.FurniturePrefab.TryGetComponent(out MeshFilter meshFilter))
-            {
-                Graphics.RenderMesh(renderParams, holoMesh, 0, Matrix4x4.TRS((hit.point + furnitureItem.FurniturePrefab.transform.position), Quaternion.Euler(0, holdingManager.Rotation, 0), Vector3.one));
-            }
+            Vector3 position = grid.HitToGrid(hit.point);
+            HologramMat.SetFloat("_OverlappingColliders", Physics.OverlapBox(position+ holoMesh.bounds.center, holoMesh.bounds.size * 0.48f, Quaternion.Euler(0, holdingManager.Rotation, 0)).Length);
+            Graphics.RenderMesh(renderParams, holoMesh, 0, Matrix4x4.TRS(position, Quaternion.Euler(0, holdingManager.Rotation, 0), Vector3.one));
         }
     }
+
+
+    /*Vector3 HitToGrid(Vector3 hitPosition)
+    {
+        Vector3 position = hitPosition;// + (grid.cellSize * 0.5f);
+        Vector3 gridPos = grid.WorldToCell(position);
+        position = Vector3.Scale(gridPos, grid.cellSize) + (grid.cellSize * 0.5f);
+        position = new Vector3(position.x, hitPosition.y, position.z);
+        return position;
+    }*/
+
+
     void OnDrawGizmosSelected() {
         if (holoMesh == null) return;
 
