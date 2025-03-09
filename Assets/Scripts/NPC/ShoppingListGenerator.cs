b3040399f9d2dec982aa87eb27d448612b912c15
Alfie Elliott <116003344+alfie-3@users.noreply.github.com>
2025-03-09 21:07:43 +0000

    Customers refuse high prices + random product quantity selection

--- Assets/Scripts/NPC/ShoppingListGenerator.cs
+++ Assets/Scripts/NPC/ShoppingListGenerator.cs
@@ -25,21 +25,45 @@ public class ShoppingListGenerator
         List<ShoppingListItem> shoppingList = new();
 
         ShopProduct_Item item = AvailableProducts[Random.Range(0, AvailableProducts.Count)];
-        int quantity = 1;
 
-        int weightedSum = 0;
-
-        foreach (WeightedProductSelectionItem weightedQuantity in item.WeightedQuantitySelection)
-        {
-            weightedSum += weightedQuantity.Entries;
-        }
-
-        int randomSelected = Random.Range(0, weightedSum);
-
-        quantity = item.WeightedQuantitySelection.FirstOrDefault(e => weightedSum >= randomSelected).Quantity;
-
-        shoppingList.Add(new(item, quantity));
+        shoppingList.Add(new(item, item.WeightedQuantitySelection.GetRandom()));
 
         return shoppingList;
     }
 }
+
+[System.Serializable]
+public class WeightedRandomBag<T>
+{
+    [System.Serializable]
+    public struct Entry
+    {
+        public double accumulatedWeight;
+        public T item;
+    }
+
+    private List<Entry> entries = new List<Entry>();
+    private double accumulatedWeight;
+    private System.Random rand = new System.Random();
+
+    public void AddEntry(T item, double weight)
+    {
+        accumulatedWeight += weight;
+        entries.Add(new Entry { item = item, accumulatedWeight = accumulatedWeight });
+    }
+
+    public T GetRandom()
+    {
+       double r = rand.NextDouble() * accumulatedWeight;
+
+        foreach (Entry entry in entries)
+        {
+            if (entry.accumulatedWeight >= r)
+            {
+                return entry.item;
+            }
+        }
+        return default(T); //should only happen when there are no entries
+    }
+}
+
