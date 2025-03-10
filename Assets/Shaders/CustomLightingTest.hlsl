c689af6a0e22a33cdbf218c82e724eb38223d44b
Greg <116003047+CalypsoGreevin@users.noreply.github.com>
2025-03-10 15:43:39 +0000

    Fixed Shadowmaps First Implementation of Point Lights interacting with Shaders

--- Assets/Shaders/CustomLightingTest.hlsl
+++ Assets/Shaders/CustomLightingTest.hlsl
@@ -13,7 +13,8 @@ void MainLightTest_float(float3 WorldPos, out float3 Direction, out float3 Color
             float4 clipPos = TransformWorldToHClip(WorldPos);
             float4 shadowCoord = ComputeScreenPos(clipPos);
         #else
-            float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+            half cascadeIndex = ComputeCascadeIndex(WorldPos);
+            float4 shadowCoord = mul(_MainLightWorldToShadow[cascadeIndex], float4(WorldPos, 1.0));
         #endif
             Light mainLight = GetMainLight(shadowCoord);
             Direction = mainLight.direction;
@@ -107,6 +108,64 @@ void AdditionalLightsTest_half(half3 SpecColor, half Smoothness, half3 WorldPosi
 
     Diffuse = diffuseColor;
     Specular = specularColor;
+}
+
+/*
+- Handles additional lights (e.g. additional directional, point, spotlights)
+- For custom lighting, you may want to duplicate this and swap the LightingLambert / LightingSpecular functions out. See Toon Example below!
+- To work in the Unlit Graph, the following keywords must be defined in the blackboard :
+	- Boolean Keyword, Global Multi-Compile "_ADDITIONAL_LIGHT_SHADOWS"
+	- Boolean Keyword, Global Multi-Compile "_ADDITIONAL_LIGHTS"
+- To support Forward+ path,
+	- Boolean Keyword, Global Multi-Compile "_FORWARD_PLUS" (2022.2+)
+*/
+void AdditionalLights_float(float3 SpecColor, float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, float4 Shadowmask,
+							out float3 Diffuse, out float3 Specular) {
+	float3 diffuseColor = 0;
+	float3 specularColor = 0;
+#ifndef SHADERGRAPH_PREVIEW
+	Smoothness = exp2(10 * Smoothness + 1);
+	uint pixelLightCount = GetAdditionalLightsCount();
+	uint meshRenderingLayers = GetMeshRenderingLayer();
+
+	#if USE_FORWARD_PLUS
+	for (uint lightIndex = 0; lightIndex < min(URP_FP_DIRECTIONAL_LIGHTS_COUNT, MAX_VISIBLE_LIGHTS); lightIndex++) {
+		FORWARD_PLUS_SUBTRACTIVE_LIGHT_CHECK
+		Light light = GetAdditionalLight(lightIndex, WorldPosition, Shadowmask);
+	#ifdef _LIGHT_LAYERS
+		if (IsMatchingLightLayer(light.layerMask, meshRenderingLayers))
+	#endif
+		{
+			// Blinn-Phong
+			float3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+			diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+			specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+		}
+	}
+	#endif
+
+	// For Foward+ the LIGHT_LOOP_BEGIN macro will use inputData.normalizedScreenSpaceUV, inputData.positionWS, so create that:
+	InputData inputData = (InputData)0;
+	float4 screenPos = ComputeScreenPos(TransformWorldToHClip(WorldPosition));
+	inputData.normalizedScreenSpaceUV = screenPos.xy / screenPos.w;
+	inputData.positionWS = WorldPosition;
+
+	LIGHT_LOOP_BEGIN(pixelLightCount)
+		Light light = GetAdditionalLight(lightIndex, WorldPosition, Shadowmask);
+	#ifdef _LIGHT_LAYERS
+		if (IsMatchingLightLayer(light.layerMask, meshRenderingLayers))
+	#endif
+		{
+			// Blinn-Phong
+			float3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+			diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+			specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, float4(SpecColor, 0), Smoothness);
+		}
+	LIGHT_LOOP_END
+#endif
+
+	Diffuse = diffuseColor;
+	Specular = specularColor;
 }
 
 #endif
