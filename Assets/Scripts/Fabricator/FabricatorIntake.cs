210563cbd825926516a29a3fb82f3f904dae786e
Toby King <91728226+MAXTHEMAD@users.noreply.github.com>
2025-04-08 19:28:24 +0100

    Fabricator

---
+++ Assets/Scripts/Fabricator/FabricatorIntake.cs
@@ -0,0 +1,45 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FabricatorIntake : NetworkBehaviour, IUseItem {
+
+    Fabricator fabricator;
+
+    private void Awake() {
+        fabricator = transform.parent.GetComponent<Fabricator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!fabricator) return;
+        if (!IsServer) return;
+
+        TryProcessInput(other.gameObject);
+
+        
+    }
+
+    public void OnItemUsed(PlayerHoldingManager holdingManager, Stock_Item shopProduct_Item) {
+        NetworkObject nwObj = holdingManager.HeldObj;
+        if (TryProcessInput(nwObj.gameObject)) {
+            holdingManager.ClearHeldItem(holdingManager.transform.position, holdingManager.transform.eulerAngles);
+        }
+    }
+
+    public bool TryProcessInput(GameObject other) {
+        if (other.TryGetComponent(out StockItem stockItem)) {
+            if (stockItem.Item == null) return false;
+            if (!stockItem.TryGetComponent(out NetworkObject nwObj)) return false;
+            if (!nwObj.IsSpawned) return false;
+            if (!fabricator.MakeItem(stockItem.Item)) return false;
+
+            /*ClipData clipData = eatingClip.GetClip(1, true);
+
+            audioSource.pitch = clipData.Pitch;
+            audioSource.PlayOneShot(clipData.Clip);*/
+            other.gameObject.GetComponent<NetworkObject>().Despawn();
+            return true;
+        } else return false;
+
+    }
+}
