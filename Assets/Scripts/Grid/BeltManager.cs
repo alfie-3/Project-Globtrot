1042a84a4b0bbd3390d5c25974c67018f9bfba25
Toby King <91728226+MAXTHEMAD@users.noreply.github.com>
2025-04-30 17:35:41 +0100

    Belt Speed Upgrade on Day 2

--- Assets/Scripts/Grid/BeltManager.cs
+++ Assets/Scripts/Grid/BeltManager.cs
@@ -11,16 +11,19 @@ public class BeltManager : NetworkBehaviour
     [field: SerializeField] public float Speed { get; private set; }
     [field: SerializeField] public float PlayerForce { get; private set; }
 
-
-
     [field: SerializeField] private List<Belt> Beltss;
-     
+
+    [field: SerializeField] protected Material BeltMaterial { get; private set; }
+
+    private float shaderRatio;
+    private float playerRatio;
 
     private void Awake() {
         if (Instance == null) {
             Instance = this;
         } else
             Destroy(this);
+
     }
 
     public static void AddMe(Belt body) {
@@ -32,5 +35,19 @@ public class BeltManager : NetworkBehaviour
 
     private void FixedUpdate() {
         Beltss.ForEach(belt => belt.Jiggle(Speed));
+    }
+
+
+    public void SetBeltSpeed(float speed) {
+        float mult = speed / Speed;
+        Speed = speed;
+        PlayerForce *= mult;
+        BeltMaterial.SetFloat("_Speed", BeltMaterial.GetFloat("_Speed")*mult);
+
+        //Beltss[0](belt => belt.GetComponent<Renderer>().sharedMaterial.SetFloat("_Speed", belt.GetComponent<Renderer>().sharedMaterial.GetFloat("_Speed") *mult));
+    }
+
+    private void OnDestroy() {
+        BeltMaterial.SetFloat("_Speed", 0.68f);
     }
 }
